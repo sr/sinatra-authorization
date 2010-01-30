22d34d19da830b18ee7746656d44cbdcb6f03412
Simon Rozet <simon@rozet.name>
2010-01-30 08:38:21 +0100

    switch to pure test/unit

--- test/authorization_test.rb
+++ test/authorization_test.rb
@@ -1,7 +1,5 @@
 require "test/unit"
 require "rack/test"
-require "context"
-require "pending"
 
 require File.dirname(__FILE__) + "/../lib/sinatra/authorization"
 
@@ -24,7 +22,7 @@ class AuthorizationApp < Sinatra::Default
 end
 
 class SinatraAuthorizationTest < Test::Unit::TestCase
-  before do
+  def setup
     @session = Rack::Test::Session.new(AuthorizationApp)
   end
 
@@ -34,32 +32,32 @@ class SinatraAuthorizationTest < Test::Unit::TestCase
     { "HTTP_AUTHORIZATION" => "Basic #{credentials}" }
   end
 
-  it "is authorized with correct credentials" do
+  def test_authorized_with_right_credentials
     @session.get "/", {}, basic_auth
     assert_equal 200, @session.last_response.status
-    assert_equal ["Welcome in protected zone"], @session.last_response.body
+    assert_equal "Welcome in protected zone", @session.last_response.body
   end
 
-  it "sets REMOTE_USER" do
-    pending "TODO"
+  def test_sets_REMOTE_USER
+    # TODO
   end
 
-  it "is unauthorized without credentials" do
+  def test_unauthorized_without_credentials
     @session.get "/"
     assert_equal 401, @session.last_response.status
   end
 
-  it "is unauthorized with incorrect credentials" do
+  def test_unauthorized_with_wrong_credentials
     @session.get "/", {}, basic_auth("evil", "wrong")
     assert_equal 401, @session.last_response.status
   end
 
-  it "returns specified realm" do
+  def test_realm
     @session.get "/"
     assert_equal %Q(Basic realm="Move on"), @session.last_response["WWW-Authenticate"]
   end
 
-  it "returns a 400, Bad Request if not basic auth" do
+  def test_no_auth
     @session.get "/", {}, { "HTTP_AUTHORIZATION" => "Foo bar" }
     assert_equal 400, @session.last_response.status
   end
